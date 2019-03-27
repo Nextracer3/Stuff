38c1b18ad66ccf82d02802763ae4a56ba2797512
Nextracer3 <PurpleBandit487@Gmail.com>
2019-03-27 18:45:15 +0000

    Player vs Computer finished, Player vs Player started

---
+++ RS2/RS2/Program.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Threading;
+
+namespace RussianRoulette2
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+
+        static void Main()
+        {
+            Console.CursorVisible = false;
+            // Intro
+
+            Console.Clear();
+            TitleTextCrawl("Russian Roulette");
+            Thread.Sleep(1000);
+
+            TextCrawl("Welcome to Russian Roulette.");
+            Thread.Sleep(1000);
+
+            TextCrawl("The rules are simple.");
+            Thread.Sleep(1000);
+
+            TextCrawl("A bullet will be placed in one of six chambers at random.");
+            Thread.Sleep(1000);
+
+            TextCrawl("Each turn, you can choose to either shoot yourself or your opponent.");
+            Thread.Sleep(1000);
+
+            TextCrawl("If you shoot your opponent and it doesn't fire, you must then shoot yourself.");
+            Thread.Sleep(1000);
+
+            TextCrawl("Once per game, you may choose to respin the cylinder and reset the bullet position.");
+            Thread.Sleep(1000);
+
+            TextCrawl("Good luck.");
+            Thread.Sleep(3000);
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+
+            // Get user input to select gamemode
+            while (true)
+            {
+                Console.Clear();
+                TextCrawl("1 -- Player vs Computer || 2 -- Player vs Player ~~>>> ", 50, false);
+                string Choice = Console.ReadLine();
+
+
+                if (Choice == "1")
+                {
+                    PvC();
+                }
+
+                else if (Choice == "2")
+                {
+                    PvP();
+                }
+
+                else
+                {
+                    TextCrawl("Invalid.");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+
+        public static void TextCrawl(string str, int ms = 50, bool endl = true)
+        {
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.Write(chars[i]);
+                Thread.Sleep(ms);
+            }
+
+            if (endl) Console.WriteLine();
+        }
+
+
+        // It may seem like this function is useless and obsolete
+        // And that would be correct
+        // But for some reason it breaks the whole program if it is removed
+        // So it is here to stay
+        public static void TitleTextCrawl(string str, int ms = 150)
+        {
+            string title = "";
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                title += chars[i];
+                Console.Title = title;
+                Thread.Sleep(ms);
+            }
+        }
+
+
+
+
+
+        public static void PvC()
+        {
+            Console.Clear();
+
+            int BulletChamber = rnd.Next(1, 7);     
+            int CurrentChamber = 1;                
+            bool GameOver = false;
+            int Round = 1;
+            bool RespinUsed = false;
+            string turn = "player";
+
+            // Game loop
+            while (!GameOver)
+            {
+                if (turn == "player")
+                {
+                    Console.Clear();
+                    Console.WriteLine("ROUND " + Round + " -- YOUR TURN");
+                    Thread.Sleep(1000);
+
+                    TextCrawl("A -- Shoot yourself || B -- Shoot opponent", 25, false);
+                    if (!RespinUsed) { TextCrawl(" || C -- Respin cylinder", 25, false); }
+                    TextCrawl(" ~~>>> ", 25, false);
+
+                    string PlayerChoice = Console.ReadLine();
+                    Console.Clear();
+
+                    if (PlayerChoice.ToLower() == "a")
+                    {
+                        if (CurrentChamber != BulletChamber)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Thread.Sleep(100);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+
+                            Console.Write("CLICK! ");
+                            Thread.Sleep(1000);
+                            TextCrawl("No bullet was fired.");
+                            Thread.Sleep(3000);
+
+                            CurrentChamber++;
+                            turn = "computer";
+                        } 
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+                            Thread.Sleep(2000);
+
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+                            TextCrawl("You are dead. You shot yourself on round " + Round + ".");
+                            Thread.Sleep(3000);
+                            GameOver = true;
+                        }
+                    }
+
+                    else if (PlayerChoice.ToLower() == "b")
+                    {
+                        if (CurrentChamber != BulletChamber)
+                        {
+                            Console.Write("CLICK! ");
+                            Thread.Sleep(1000);
+                            TextCrawl("No bullet was fired - you must now shoot yourself...");
+                            Thread.Sleep(2000);
+
+                            CurrentChamber++;
+
+                            if (CurrentChamber != BulletChamber)
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.Clear();
+                                Thread.Sleep(100);
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.Clear();
+
+                                Console.Write("CLICK! ");
+                                Thread.Sleep(1000);
+                                TextCrawl("No bullet was fired.");
+                                Thread.Sleep(3000);
+
+                                CurrentChamber++;
+                                turn = "computer";
+                            }
+
+                            else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.Clear();
+                                Console.WriteLine("BANG!");
+                                Thread.Sleep(2000);
+
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.Clear();
+                                Console.WriteLine("BANG!");
+                                TextCrawl("You are dead. You shot yourself on round " + Round + ".");
+                                Thread.Sleep(3000);
+                                GameOver = true;
+                            }
+                        }
+
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+                            Thread.Sleep(2000);
+
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+                            TextCrawl("Your opponent is dead. You shot them on round " + Round + ".");
+                            Thread.Sleep(3000);
+                            GameOver = true;
+                        }
+                    }
+
+                    else if (PlayerChoice.ToLower() == "c" && !RespinUsed)
+                    {
+                        TextCrawl("Respinning the cylinder.");
+                        BulletChamber = rnd.Next(1, 7);
+                        CurrentChamber = 1;
+
+                        Thread.Sleep(1000);
+                        TextCrawl("Cylinder has been randomized.");
+                        Thread.Sleep(2000);
+
+                        turn = "computer";
+                    }
+
+                    else
+                    {
+                        TextCrawl("Invalid selection.");
+                        Thread.Sleep(2000);
+
+                        turn = "computer";
+                    }
+
+                    Round++;
+                }
+
+                // Computer turn
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("ROUND " + Round + " -- COMPUTER'S TURN");
+                    Thread.Sleep(1000);
+
+                    TextCrawl("Computer is making a selection.");
+                    Thread.Sleep(3000);    // Just for suspense
+
+                    int ComputerChoice = rnd.Next(1, 3);
+
+                    if (ComputerChoice == 1)
+                    {
+                        Console.Clear();
+                        TextCrawl("Computer has chosen to shoot itself.");
+                        Thread.Sleep(2000);
+
+                        if (CurrentChamber != BulletChamber)
+                        {
+                            Console.Clear();
+                            Console.Write("CLICK! ");
+                            Thread.Sleep(1000);
+                            TextCrawl("No bullet was fired.");
+                            Thread.Sleep(2000);
+
+                            CurrentChamber++;
+                            turn = "player";
+                        }
+
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+
+                            Console.WriteLine("BANG!");
+                            Thread.Sleep(2000);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+
+                            TextCrawl("Your opponent is dead. They shot themselves on round " + Round + ".");
+                            Thread.Sleep(3000);
+                            GameOver = true;
+                        }
+                    }
+
+                    else if (ComputerChoice == 2)
+                    {
+                        Console.Clear();
+                        TextCrawl("Computer has chosen to shoot you.");
+                        Thread.Sleep(2000);
+
+                        if (CurrentChamber != BulletChamber)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Thread.Sleep(100);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+
+                            Console.Write("CLICK! ");
+                            Thread.Sleep(1000);
+                            TextCrawl("No bullet was fired - they must now shoot themselves...");
+                            Thread.Sleep(2000);
+
+                            CurrentChamber++;
+
+                            if (CurrentChamber != BulletChamber)
+                            {
+                                Console.Write("CLICK! ");
+                                Thread.Sleep(1000);
+                                TextCrawl("No bullet was fired.");
+                                Thread.Sleep(2000);
+
+                                CurrentChamber++;
+                                turn = "player";
+                            }
+
+                            else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.Clear();
+                                Console.WriteLine("BANG!");
+                                Thread.Sleep(2000);
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.Clear();
+                                Console.WriteLine("BANG!");
+
+                                TextCrawl("Your opponent is dead. They shot themselves on round " + Round + ".");
+                                Thread.Sleep(3000);
+                                GameOver = true;
+                            }
+                        }
+
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+                            Thread.Sleep(2000);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.WriteLine("BANG!");
+
+                            TextCrawl("You are dead. Your opponent shot you on round " + Round + ".");
+                            Thread.Sleep(3000);
+                            GameOver = true;
+                        }
+                    }
+                }
+
+                Round ++;
+            }
+
+
+            // Game over
+
+            Console.Clear();
+            TextCrawl("Would you like to play again?");
+            TextCrawl("1 -- Player vs Computer || 2 -- Player vs Player || 3 -- Exit");
+            TextCrawl("\nPress a key to make a selection.");
+
+            var key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.D1) { PvC(); }
+            else if (key.Key == ConsoleKey.D2) { PvP(); }
+            else { Environment.Exit(0); }
+        }
+
+
+
+
+
+        public static void PvP()
+        {
+            Console.Clear();
+            TextCrawl("Enter Player 1's name ~~>>> ", 50, false);
+            string Player1Name = Console.ReadLine();
+
+            TextCrawl("Enter Player 2's name ~~>>> ", 50, false);
+            string Player2Name = Console.ReadLine();
+
+            Console.Clear();
+            Console.Write(Player1Name);
+            Thread.Sleep(1000);
+            Console.Write("        VS");
+            Thread.Sleep(1000);
+            Console.Write("        {0}", Player2Name);
+            Thread.Sleep(3000);
+
+            int BulletChamber = rnd.Next(1, 7);
+            int CurrentChamber = 1;
+            bool GameOver = false;
+            bool P1RespinUsed = false;
+            bool P2RespinUsed = false;
+            int Round = 1;
+            string[] TurnIndex = { "player1", "player2" };
+            string turn = TurnIndex[rnd.Next(TurnIndex.Length)];    // Randomly select player to start
+
+
+            //Game loop
+            while (!GameOver)
+            {
+                //Player 1 turn
+                if (turn == "player1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("ROUND {0} -- {1}'s TURN", Round, Player2Name);
+                    Thread.Sleep(1000);
+
+                    TextCrawl("A -- Shoot yourself || B -- Shoot opponent ", 25, false);
+                    if (!P1RespinUsed) { TextCrawl("|| C -- Respin cylinder", 25, false); }
+                }
+
+                // Player 2 turn
+                else
+                {
+
+                }
+            }
+
+
+            // Game Over
+
+
+        }
+    }
+}

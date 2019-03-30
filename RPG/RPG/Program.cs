bebe61d5d1f21a71f9797ff0cd5fe843f19b051a
Nextracer3 <PurpleBandit487@Gmail.com>
2019-03-30 21:00:13 +0000

    basic functions

--- RPG/RPG/Program.cs
+++ RPG/RPG/Program.cs
@@ -13,5 +13,47 @@ namespace RPG
 
 
         }
+
+
+        public static void TextCrawl(string str, int ms = 50, bool endl = true)
+        {
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.Write(chars[i]);
+                Thread.Sleep(ms);
+            }
+
+            if (endl) Console.WriteLine();      // Leave a new line if endl is true
+        }
+
+
+        public static void TitleTextCrawl(string str, int ms = 150)
+        {
+            char[] chars = str.ToCharArray();
+            string title = "";
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                title += chars[i];
+                Console.Title = title;
+                Thread.Sleep(ms);
+            }
+        }
+
+
+        public static void SetCol(ConsoleColor colour, string ground = "fore")      // Because I will be changing colours a lot
+        {
+            if (ground == "fore")
+            {
+                Console.ForegroundColor = colour;
+            }
+            else
+            {
+                Console.BackgroundColor = colour;
+            }
+        }
+        
     }
 }

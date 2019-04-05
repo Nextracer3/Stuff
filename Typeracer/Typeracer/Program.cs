90fd76ab3492ba8b35a5184746ed9ef84376c4a8
Nextracer3 <PurpleBandit487@Gmail.com>
2019-04-05 19:12:21 +0100

    init

---
+++ Typeracer/Typeracer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace TypeRacer
+{
+    class Program
+    {
+        static void Main(string[] cmdLineArgs)
+        {
+            if (cmdLineArgs.Length > 0) { Console.WriteLine("ARGS: {0}", cmdLineArgs); }
+
+
+        }
+    }
+}
